7278d1fb64f62391b9bfb1311dd5f15a17deef1b
EmmanuelRamosBiado <ramosbiadoe@gmail.com>
2023-06-15 18:37:05 +0200

    Initial Commit

---
+++ var/cache/prod/appParameters.php
@@ -0,0 +1,25 @@
+<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => '127.0.0.1',
+    'database_port' => '3306',
+    'database_name' => 'bitnami_prestashop',
+    'database_user' => 'bn_prestashop',
+    'database_password' => 'd68c80a1390d0b5ba37010d8b72790cdcf0dc657cd0319371a5df1fa00332902',
+    'database_prefix' => 'ps_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => 'xL41uofyT7VxtIVFLfvSa8pyr7tDbRxbfKeHU2IvkY5YP3uI6WCqZpwE90SavgSA',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2023-06-08',
+    'locale' => 'en-US',
+    'use_debug_toolbar' => true,
+    'cookie_key' => 'bt5e2KEFLMAiajF6vVv5SZq0Vd5BGfo11zIDiPoYPii1ZaNPCp74bkkuPAzp7FEx',
+    'cookie_iv' => '0SYFq0482baiQWo7KcZGnPmahSFhgwaO',
+    'new_cookie_key' => 'def000000911cd5ca535978b1cb54dea81e7813d423e6c9d575fc9f96163be866c763d10618d9312e840e43eb34f035f7d442fdc6bc07bc7962b2e5a49f898879195ffb1',
+  ),
+);
